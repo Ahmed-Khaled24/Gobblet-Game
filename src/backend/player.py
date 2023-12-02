0461ac9fb58a5246e9fd0a664f4bda3fa4fb7ef6
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-12-02 13:18:42 +0200

    feat: update PieceSize and Color enums

--- src/backend/player.py
+++ src/backend/player.py
@@ -7,10 +7,26 @@ class PieceSize(Enum):
     MEDIUM: int = 2
     LARGE: int = 3
 
+    
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value < other.value
+        return NotImplemented
+
+    def __gt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value > other.value
+        return NotImplemented
+
 
 class Color(Enum):
     WHITE: int = 0
     BLACK: int = 1
+    
+    def __eq__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value == other.value
+        return NotImplemented
 
 
 class Piece(object):
