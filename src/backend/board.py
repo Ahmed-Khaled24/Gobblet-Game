a43f4d03d969ac762b50fd699fdcf32ef954b7d4
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-12-14 23:58:04 +0200

    feat: add new function getLInedUpGobblets
    This function helps to get the position of lined up gobblets in lines of 3 or 4 pieces.
    Which helps to decide if the player can add new gobblet from the external stack at this position or not.
    Also it will be used to determine the win state.

--- src/backend/board.py
+++ src/backend/board.py
@@ -1,5 +1,6 @@
 from src.backend.player import Piece
 from src.utils.invalidMoveException import InvalidMoveException
+from src.backend.player import Color
 
 class Board:
     # Use list as a stack push === append, pop === pop 
@@ -18,11 +19,84 @@ class Board:
         if self.grid[row][column][-1] is None:
             self.grid[row][column].append(piece)
         else:
-            existingPiece = self.grid[row][column][-1]
-            if piece.size > existingPiece.size:
-                self.grid[row][column].append(piece)
+            linedUpGobblets = self.getLinedUpGobblets(Color.WHITE if piece.color == Color.BLACK else Color.BLACK)
+            if len(linedUpGobblets) > 0 and len([gobblet for gobblet in linedUpGobblets if (row, column) in gobblet]) > 0:
+                existingPiece = self.grid[row][column][-1]
+                if piece.size > existingPiece.size:
+                    self.grid[row][column].append(piece)
+                else:
+                    raise InvalidMoveException("Invalid move, you can't add your piece on top of larger piece.")
             else:
-                raise InvalidMoveException("Invalid move, you can't add your piece on top of larger piece.")
+                raise InvalidMoveException("Invalid move, you can't add your piece on top of another piece, unless there are 3 lined up.") 
+    
+    
+    def getLinedUpGobblets(self, color: Color) -> list[list[tuple[int, int]]]:
+        """ Returns a list of tuples containing the coordinates of the lined up gobblets """
+        linedUpGobbles = []
+        
+        # Count the total number of gobblets of the given color, if less than 3, then we don't care yet.
+        count = 0
+        for row in range(4):
+            for column in range(4):
+                if self.grid[row][column][-1] is not None and self.grid[row][column][-1].color == color:
+                    count += 1
+        
+        if count < 3:
+            return linedUpGobbles
+        
+        # Scan rows
+        for row in range(4):
+            temp = []
+            for column in range(4):
+                if self.grid[row][column][-1] is not None and self.grid[row][column][-1].color == color:
+                    temp.append((row, column))
+            
+            if len(temp) == 4:
+                linedUpGobbles.append(temp)
+            elif len(temp) == 3:
+                # Check if the 3 lined up are adjacent
+                if temp[0][1] + 1 == temp[1][1] and temp[1][1] + 1 == temp[2][1]:
+                    linedUpGobbles.append(temp)
+            
+        # Scan columns
+        for column in range(4):            
+            temp = []
+            for row in range(4):
+                if self.grid[row][column][-1] is not None and self.grid[row][column][-1].color == color:
+                    temp.append((row, column))
+            
+            if len(temp) == 4:
+                linedUpGobbles.append(temp)
+            elif len(temp) == 3:
+                # Check if the 3 lined up are adjacent
+                if temp[0][0] + 1 == temp[1][0] and temp[1][0] + 1 == temp[2][0]:
+                    linedUpGobbles.append(temp)
+             
+        #Scan diagonals
+        def scanDiagonal(startRow: int, startColumn:int, rowLimit:int, columnLimit:int, acceptedLength:int, leftDiagonal:bool):
+            temp = []
+            row = startRow
+            column = startColumn
+            while ((row >= rowLimit) or (leftDiagonal and row <= rowLimit)) and column <= columnLimit:
+                if self.grid[row][column][-1] is not None and self.grid[row][column][-1].color == color:
+                    temp.append((row, column))
+                row = row+1 if leftDiagonal else row-1
+                column += 1
+            
+            if len(temp) == acceptedLength:
+                linedUpGobbles.append(temp)       
+        
+        # Right diagonals
+        scanDiagonal(startRow=2, startColumn=0, rowLimit=0, columnLimit=2, acceptedLength=3, leftDiagonal=False)
+        scanDiagonal(startRow=3, startColumn=0, rowLimit=0, columnLimit=3, acceptedLength=4, leftDiagonal=False)
+        scanDiagonal(startRow=3, startColumn=1, rowLimit=1, columnLimit=3, acceptedLength=3, leftDiagonal=False)
+        
+        # Left diagonals
+        scanDiagonal(startRow=1, startColumn=0, rowLimit=3, columnLimit=2, acceptedLength=3, leftDiagonal=True)
+        scanDiagonal(startRow=0, startColumn=0, rowLimit=3, columnLimit=3, acceptedLength=4, leftDiagonal=True)
+        scanDiagonal(startRow=0, startColumn=1, rowLimit=2, columnLimit=3, acceptedLength=3, leftDiagonal=True)
+        
+        return linedUpGobbles
         
         
     def movePiece(self, oldRow: int, oldColumn: int, newRow: int, newColumn: int):
@@ -33,7 +107,14 @@ class Board:
         
         self.grid[oldRow][oldColumn].pop()
         
-        self.addPiece(newRow, newColumn, piece)
+        if self.grid[newRow][newColumn][-1] is None:
+            self.grid[newRow][newColumn].append(piece)
+        else:
+            existingPiece = self.grid[newRow][newColumn][-1]
+            if piece.size > existingPiece.size:
+                self.grid[newRow][newColumn].append(piece)
+            else:
+                raise InvalidMoveException("Invalid move, you can't add your piece on top of larger piece.")
         
 
 if __name__ == "__main__":
