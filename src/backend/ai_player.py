76c04192a1f1b756d45a1aab68b130976f7f0c6d
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2024-01-11 01:23:38 +0200

    feat: update AI Class
    add difficulty to the constructor of the AI class

--- src/backend/ai_player.py
+++ src/backend/ai_player.py
@@ -4,10 +4,11 @@ from src.backend.player import *
 from uuid import uuid1
 
 class AI(Player):
-    def __init__(self, color: Color):
+    def __init__(self, color: Color, difficulty: int = 2):
         Player.__init__(self, color)
         self.icon = None
         self.id = "AI: "+uuid.uuid1().__str__()
+        self.difficulty = difficulty
 
     def __str__(self):
         return f"AI: Color({self.color})"
