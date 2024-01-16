092c682d0da6fe8b5ab97e8ef4c02a8a87aaff28
ahmedelsayed968 <ahmedeltop968@gmail.com>
2024-01-16 18:21:54 +0200

    feat: unit tests for scoring function for heuristic v2

--- tests/test_Eval.py
+++ tests/test_Eval.py
@@ -21,3 +21,39 @@ def test_check_uniqueness():
     assert Evaluation.check_uniqueness(pieces3) is True
     assert Evaluation.check_uniqueness(pieces4) is True
     assert Evaluation.check_uniqueness(pieces5) is True
+
+
+def test_get_pieces_score():
+    eval = Evaluation()
+    board = Board()
+    ai = AI(color=Color.WHITE)
+    person = Person(color=Color.BLACK)
+    eval.board, eval.max, eval.min = board, ai, person
+    pieces1 = [Piece(Color.WHITE, PieceSize.LARGE),
+               Piece(Color.WHITE, PieceSize.LARGE),
+               Piece(Color.BLACK, PieceSize.SMALL),
+               None]
+
+    pieces2 = [Piece(Color.WHITE, PieceSize.LARGE),
+               Piece(Color.WHITE, PieceSize.LARGE),
+               Piece(Color.BLACK, PieceSize.LARGE),
+               None]
+
+    pieces3 = [Piece(Color.WHITE, PieceSize.LARGE),
+               Piece(Color.WHITE, PieceSize.LARGE),
+               Piece(Color.BLACK, PieceSize.SMALL),
+               Piece(Color.BLACK, PieceSize.SMALL)]
+    pieces4 = [Piece(Color.WHITE, PieceSize.LARGE),
+               Piece(Color.WHITE, PieceSize.LARGE),
+               None,
+               None]
+    pieces5 = [Piece(Color.BLACK, PieceSize.LARGE),
+               Piece(Color.BLACK, PieceSize.LARGE),
+               Piece(Color.BLACK, PieceSize.LARGE),
+               None]
+    assert eval.get_pieces_score(pieces1) == PieceSize.LARGE.value ** 2 * 10**2
+    assert eval.get_pieces_score(pieces2) == 0
+    assert eval.get_pieces_score(pieces3) == 0
+
+    assert eval.get_pieces_score(pieces4) == pieces4[0].size.value * 10 * pieces4[1].size.value * 10
+    assert eval.get_pieces_score(pieces5) == -1 * PieceSize.LARGE.value ** 3 * 10**3
